0d9e126ea1bbf5b06523e3fb15768d8187bdfbc4
Simran jeet Singh <simranj674@gmail.com>
2020-09-08 16:31:23 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/git@github.com:Odin-007/Find-your-Recipe.git/precache-manifest.a28a129d44e64866c93b69cc269a7976.js"
+  "/Odin-007/Find-your-Recipe.git/precache-manifest.ee0ce4e3385dc675d331d9030826fc74.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/git@github.com:Odin-007/Find-your-Recipe.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Odin-007/Find-your-Recipe.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
